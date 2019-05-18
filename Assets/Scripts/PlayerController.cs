dd2c32f4f252b12d9a6949dd47da10d211ff6a51
FiroGX <firogx@gmail.com>
2019-05-18 12:45:46 +0100

    PlayerController added but not final

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera camera;
+
+    float camera_rotation;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera_rotation = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camera.transform.position.Set(this.transform.position.x + 3, this.transform.position.y + 3, this.transform.position.z + 3);
+        camera.transform.LookAt(this.transform); // para olhar para o player
+
+
+    }
+}
